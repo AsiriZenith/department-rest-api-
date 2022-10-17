56d8a1fb505f6fe623c508e36a0fac26024a3430
AsiriZenith <asirisenith@gmail.com>
2022-10-17 23:32:47 +0530

    get & post method implemented

---
+++ Spring-boot-tutorial-dailycodebuffer/src/main/java/com/dailycodebuffer/Springboottutorial/service/DepartmentService.java
@@ -0,0 +1,10 @@
+package com.dailycodebuffer.Springboottutorial.service;
+
+import com.dailycodebuffer.Springboottutorial.entity.Department;
+
+import java.util.List;
+
+public interface DepartmentService {
+    public Department saveDepartment(Department department);
+    public List<Department> fetchDepartmentList();
+}
---
+++ Spring-boot-tutorial-dailycodebuffer/src/main/java/com/dailycodebuffer/Springboottutorial/service/DepartmentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.dailycodebuffer.Springboottutorial.service;
+
+import com.dailycodebuffer.Springboottutorial.entity.Department;
+import com.dailycodebuffer.Springboottutorial.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+
+    @Override
+    public List<Department> fetchDepartmentList() {
+        return departmentRepository.findAll();
+    }
+}
